00af8a99ee01a84180461406a8dfecb256e05188
roseberr <bsc981111@gmail.com>
2022-07-04 09:50:59 +0900

    first commit

---
+++ MyOwnHomepage/src/main/java/homepage/MyOwnHomepage/MyOwnHomepageApplication.java
@@ -0,0 +1,13 @@
+ package homepage.MyOwnHomepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyOwnHomepageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyOwnHomepageApplication.class, args);
+	}
+
+}
---
+++ MyOwnHomepage/src/test/java/homepage/MyOwnHomepage/MyOwnHomepageApplicationTests.java
@@ -0,0 +1,13 @@
+package homepage.MyOwnHomepage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyOwnHomepageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
